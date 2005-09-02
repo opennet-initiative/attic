ad5742b4472831837b3ae020bb2f8f6c75d6c26a
rene <rene>
2005-09-02 10:31:42 +0000

    Added portmapping interface

--- target/default/target_skeleton/etc/openvpn/opennet_up.sh
+++ target/default/target_skeleton/etc/openvpn/opennet_up.sh
@@ -6,3 +6,4 @@ ip route add throw $WIFINET/$WIFIPRE table 3
 ip route add default via $route_vpn_gateway table 3
 iptables -t nat -A POSTROUTING -o $dev -s $LANNET/$LANPRE -j SNAT --to-source $ifconfig_local
 echo "vpn-tunnel active" >/tmp/openvpn_msg.txt	# a short message for the web frontend
+/etc/init.d/portmapping start
