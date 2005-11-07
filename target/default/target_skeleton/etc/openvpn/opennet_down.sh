8ff7bfb4108c3cb43ae574c82dbdea0b335f1a9a
rene <rene>
2005-11-07 21:05:13 +0000

    portmapping not related to openvpn anymore, so removed portmapping-related lines

--- target/default/target_skeleton/etc/openvpn/opennet_down.sh
+++ target/default/target_skeleton/etc/openvpn/opennet_down.sh
@@ -2,6 +2,3 @@
 eval $(/usr/bin/netparam)
 iptables -t nat -D POSTROUTING -o $dev -s $LANNET/$LANPRE -j SNAT --to-source $ifconfig_local
 rm -f /tmp/openvpn_msg.txt	# remove running message
-export tundev=$dev
-export tunipaddr=$ifconfig_local
-/etc/init.d/portmapping stop
