a00fbc27872a6551bb5305a23acfe29f3de76684
rene <rene>
2006-05-27 13:57:04 +0000

    nagare's ip is now retrieved from test-ping replaced ppp0 by $WANDEV

--- trunk/etc/openvpn/opennet_dsl/opennet_dsl_down.sh
+++ trunk/etc/openvpn/opennet_dsl/opennet_dsl_down.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 eval $(/usr/bin/netparam)
 
-# get ip of nagare from routing table 5
-# table 5 and the following iptable-rules should have alsways the same nagare-ip
-# if nagare-ip will change, this will be recognized by cron.hourly
-nagare_ip=$(ip route show table 5 | cut -d' ' -f1)
+nagare_ip=$(ping -c 1 nagare.on-i.de 2>/dev/null | grep PING);
+nagare_ip=${nagare_ip#*\(};
+nagare_ip=${nagare_ip%%\)*};
 
 # dont use dsl-tunnel for user-tunneld packages
-iptables -t nat -D POSTROUTING -o ppp0 -s $WIFINET/$WIFIPRE -d $nagare_ip -p udp --dport 1600 -j SNAT --to-source $WANADR
+iptables -t nat -D POSTROUTING -o $WANDEV -s $WIFINET/$WIFIPRE -d $nagare_ip -p udp --dport 1600 -j SNAT --to-source $WANADR
 iptables -t nat -D PREROUTING -d 192.168.0.251  -p udp --dport 1600 -j DNAT --to-destination $nagare_ip
 
 rm -f /tmp/openvpn_dsl_msg.txt	# removing running message
