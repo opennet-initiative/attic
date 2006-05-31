c80e2d55009b57eaa495c0e8cb17f0b6a105135e
rene <rene>
2006-05-31 23:15:29 +0000

    release 'umber hulk' - user-gateway support

---
+++ tags/release-0.11ipkg-1/etc/openvpn/opennet_user/opennet_up.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+eval $(/usr/bin/netparam)
+ip route flush table 3
+ip route add throw $LANNET/$LANPRE table 3
+ip route add throw $WIFINET/$WIFIPRE table 3
+ip route add default via $route_vpn_gateway table 3
+iptables -t nat -A POSTROUTING -o $dev -s $LANNET/$LANPRE -j SNAT --to-source $ifconfig_local
+iptables -t nat -A POSTROUTING -o $dev -s $DHCPWIFINET/$DHCPWIFIPRE -j SNAT --to-source $ifconfig_local
+echo "vpn-tunnel active" >/tmp/openvpn_msg.txt	# a short message for the web frontend
