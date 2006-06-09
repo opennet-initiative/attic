7a845c21792c2c3979994216562b87579c608914
rene <rene>
2006-06-09 21:57:56 +0000

    break if firmware-directory-listing could not retrieved

--- trunk/usr/sbin/get_firmware_list.sh
+++ trunk/usr/sbin/get_firmware_list.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 # hole aktuelle paketliste
-wget -q -O /tmp/firmware-packages.html http://www.opennet-initiative.de/firmware/packages/ 2>/dev/null; fi
+wget -q -O /tmp/firmware-packages.html http://www.opennet-initiative.de/firmware/packages/ 2>/dev/null;
+if [ $? != 0 ] || ! [ -f /tmp/firmware-packages.html ]; then
+	# no information recived
+	rm -rf /tmp/firmware-packages.html; exit
+fi
 
 # opennet-firmware_0.10ipkg-1pre_mipsel.ipk pakete mit dem suffix 'pre' werden in der Auswahlliste des Webfrontends nicht angezeigt
 
 recent=$(ipkg list_installed opennet-firmware | awk 'BEGIN {FS="-|\\."} /opennet-firmware/ {str = sprintf("%03d%03d%03d%03d", $3, $4, $5, $6); print str }')
 
-if [ -e /tmp/firmware-packages.html ]; then
-	awk '
-		BEGIN {RS="href=\"|\">";FS="_|-|\\."}
-		/opennet-firmware/ && /ipkg/ && $0 !~ "info|md5|pre|se505" {
-			printf("%03d%03d%03d%03d:%s\n", $3, $4, $5, $6, $0)
-		}' /tmp/firmware-packages.html | \
-			sort -nr >/tmp/firmware-packages
-fi
+awk '
+	BEGIN {RS="href=\"|\">";FS="_|-|\\."}
+	/opennet-firmware/ && /ipkg/ && $0 !~ "info|md5|pre|se505" {
+		printf("%03d%03d%03d%03d:%s\n", $3, $4, $5, $6, $0)
+	}' /tmp/firmware-packages.html | \
+		sort -nr >/tmp/firmware-packages
 
 # check if newest found package is newer than installed
 best=$(awk 'BEGIN {FS=":"} {print $1; exit}' /tmp/firmware-packages)
