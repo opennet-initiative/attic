981b313b0a10389ca765db4fbc5920693434c20e
rene <rene>
2006-06-09 19:13:50 +0000

    wrong chain choosen

--- trunk/etc/openvpn/opennet_dsl/opennet_dsl_down.sh
+++ trunk/etc/openvpn/opennet_dsl/opennet_dsl_down.sh
@@ -8,6 +8,6 @@ RULENUM=$(iptables -L POSTROUTING -t nat --line-numbers -n | awk "/$WIFINET\/$WI
 iptables -D POSTROUTING $RULENUM -t nat
 
 RULENUM=$(iptables -L PREROUTING -t nat --line-numbers -n | awk '/192.168.0.251/ && /dpt:1600/ {print $1; exit}')
-iptables -D POSTROUTING $RULENUM -t nat
+iptables -D PREROUTING $RULENUM -t nat
 
 rm -f /tmp/openvpn_dsl_msg.txt	# removing running message
