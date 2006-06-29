d2e0ed362289670670b4e123e23af6491328585e
rene <rene>
2006-06-29 18:42:28 +0000

    changed to get IP of nagare with nslookup instead of ping

--- trunk/etc/openvpn/opennet_dsl/opennet_dsl_up.sh
+++ trunk/etc/openvpn/opennet_dsl/opennet_dsl_up.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 eval $(/usr/bin/netparam)
 
-nagare_ip=$(ping -c 1 nagare.on-i.de 2>/dev/null | grep PING);
-nagare_ip=${nagare_ip#*\(};
-nagare_ip=${nagare_ip%%\)*};
+nagare_ip=$(nslookup nagare.on-i.de 2>/dev/null | tail -n 1 | awk '{ print $2 }')
+#nagare_ip=$(ping -c 1 nagare.on-i.de 2>/dev/null | grep PING);
+#nagare_ip=${nagare_ip#*\(};
+#nagare_ip=${nagare_ip%%\)*};
 
 # dont use dsl-tunnel for user-tunneld packages
 iptables -t nat -A POSTROUTING -o $WANDEV -s $WIFINET/$WIFIPRE -d $nagare_ip -p udp --dport 1600 -j SNAT --to-source $WANADR
