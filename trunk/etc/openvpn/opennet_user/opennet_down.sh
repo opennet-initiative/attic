5a57d7c781ca197b83129cf3956654938b27a44f
rene <rene>
2006-07-17 22:44:06 +0000

    removed netparam-dependency. (was weak, possible reason for problems)

--- trunk/etc/openvpn/opennet_user/opennet_down.sh
+++ trunk/etc/openvpn/opennet_user/opennet_down.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-eval $(/usr/bin/netparam)
-iptables -t nat -D POSTROUTING -o $dev -s $LANNET/$LANPRE -j SNAT --to-source $ifconfig_local
-iptables -t nat -D POSTROUTING -o $dev -s $DHCPWIFINET/$DHCPWIFIPRE -j SNAT --to-source $ifconfig_local
+
+# simply remove all SNAT-rules for the opennet_user tunnel
+get_rulenum() {
+	iptables -L POSTROUTING -t nat --line-numbers -n -v | awk '$4 == "SNAT" && $8 == "tun0" {print $1; exit}'
+}
+while [ -n "$(get_rulenum)" ]; do
+	iptables -D POSTROUTING $(get_rulenum) -t nat
+done
+
 rm -f /tmp/openvpn_msg.txt	# remove running message
