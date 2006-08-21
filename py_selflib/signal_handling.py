a566f9733e23fd69afb6c02cdc71f2f4aa675919
sebastian <sebastian>
2006-08-21 01:46:09 +0000

    made signal_handling.py slightly less eucharis-specific

--- py_selflib/signal_handling.py
+++ py_selflib/signal_handling.py
@@ -6,15 +6,19 @@ import sys
 import logging
 import thread
 
-import data_pickling
 import socket_management
+try:
+   import data_pickling
+except ImportError:
+   data_pickling = None
 
 logger = logging.getLogger('signal_handling')
 
 def program_shutdown(signal, stack_frame):
    logger.log(50, 'Caught signal %s, shutting down.' % (signal,))
    thread.start_new_thread(socket_management.shutdown, ())
-   data_pickling.save_variables()
+   if (data_pickling):
+      data_pickling.save_variables()
    sys.exit(signal)
 
 
