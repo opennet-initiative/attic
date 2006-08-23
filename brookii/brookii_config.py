c6dc70fea0468d85644d38b48b30dcf1bf715849
sebastian <sebastian>
2006-08-23 18:47:08 +0000

    slight adjustment to default brookii config

--- brookii/brookii_config.py
+++ brookii/brookii_config.py
@@ -2,14 +2,20 @@
 
 import logging.handlers
 
-target_uid = 1000
+target_uid = FIXME
 target_gid = target_uid
 
 settings = {
    'basic_io': {
       'handlers':(
-         ([None], logging.handlers.RotatingFileHandler, (), {'filename':'log/all'}, (('setLevel', (20,)),)),
-         ([None], logging.StreamHandler, (), {}, (('setLevel', (20,)),))
+         ([None], logging.handlers.RotatingFileHandler, (), {'filename':'log/all', 'maxBytes':1048576, 'backupCount':1}, (('setLevel', (20,)),)),
+         #([None], logging.StreamHandler, (), {}, (('setLevel', (20,)),))
          )
       },
   }
+
+db_kwargs = {
+   'db':'brookii',
+   'user':'brookii',
+   'passwd':FIXME
+}
