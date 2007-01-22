6b07a5ca205c9b30542292daa22fd11e72094d82
rene <rene>
2007-01-22 14:56:26 +0000

    tip that new firmware is available is now done with cgi. no css-trick needed anymore

--- trunk/usr/sbin/get_firmware_list.sh
+++ trunk/usr/sbin/get_firmware_list.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# aktualisiere die ipkg-paketliste (um mögliche Abhängigkeiten zu bereinigen)
+ipkg update >/dev/null 2>/dev/null
+
 # hole aktuelle paketliste
 wget -q -O /tmp/firmware-packages.html http://www.opennet-initiative.de/firmware/packages/ 2>/dev/null;
 if [ $? != 0 ] || ! [ -f /tmp/firmware-packages.html ]; then
@@ -17,6 +20,7 @@ awk '
 		printf("%03d%03d%03d%03d:%s\n", $3, $4, $5, $6, $0)
 	}' /tmp/firmware-packages.html | \
 		sort -nr >/tmp/firmware-packages
+rm /tmp/firmware-packages.html
 
 # check if newest found package is newer than installed
 best=$(awk 'BEGIN {FS=":"} {print $1; exit}' /tmp/firmware-packages)
@@ -24,5 +28,5 @@ recent=$(ipkg list_installed opennet-firmware | awk 'BEGIN {FS="-|\\."} /opennet
 
 # if there is not the newest version on the ap, show that on the frontpage
 if [ $best -gt $recent ]; then
-	echo "#firmware_update {display: inline}" >/www/on.css
-else rm -f /www/on.css; fi
+	echo "firmware_update available" >/tmp/firmware-update-available
+else rm -f /tmp/firmware-update-available; fi
