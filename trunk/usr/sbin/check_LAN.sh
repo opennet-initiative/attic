4bf191899d5d63cb9316c9b3d2466befd27819a2
rene <rene>
2007-03-16 23:44:30 +0000

    moved LAN-Warning from cgi-bin-status into check_LAN

--- trunk/usr/sbin/check_LAN.sh
+++ trunk/usr/sbin/check_LAN.sh
@@ -7,12 +7,13 @@ startswith() { local str="$1"; local substr="$2";
 
 lan_addr="$(ifconfig $(nvram get lan_ifname) 2>/dev/null| awk 'BEGIN{FS=" +|:"} $2 == "inet" {print $4; exit}')"
 
+if [ -n "$(grep $(nvram get lan_ifname) /var/etc/olsrd.conf)" ]; then
+	warn="<b>ACHTUNG:</b> LAN ist mit IP $lan_addr Teil des Opennet. OLSR auf LAN aktiviert."
 
-if [ -n "$lan_addr" ] && [ $(startswith "$lan_addr" "172.16.0") = 0 ]; then
+elif [ -n "$lan_addr" ] && [ $(startswith "$lan_addr" "172.16.0") = 0 ]; then
 	warn="<b>ACHTUNG:</b> Dein AccessPoint hat im LAN die Adresse $lan_addr. Zukünftige Firmware-Versionen werden den für LAN verfügbaren Adressbereich einschränken. Achte genau auf die Ausgaben bei Firmware-Aktualisierungen oder versuche bereits jetzt die Adresse ins Netz 172.16.0.X (Netzmaske 255.255.255.0) zu legen, also bspw. 172.16.0.1."
-fi
 
-if [ -z "$warn" ]; then
+else
 	lan_addrs="$(cat /proc/net/arp | awk '$6 == "'$(nvram get lan_ifname)'" {print $1}')"
 
 	for addr in $lan_addrs; do
