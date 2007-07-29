43efc861b5768283e53545f58c9250ebff1f816f
sebastian <sebastian>
2007-07-29 14:26:01 +0000

    alfredi kml output fixes: apparently google changed their format spec ...

--- alfredi/alfredi_output_kml.py
+++ alfredi/alfredi_output_kml.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#Copyright 2006 Sebastian Hagen
+#Copyright 2006, 2007 Sebastian Hagen
 # This file is part of alfredi.
 
 # alfredi is free software; you can redistribute it and/or modify
@@ -34,24 +34,25 @@ class KMLPlacemark:
          sep = ''
       return sep.join(('<%s%s>' % (name, arguments), xml.sax.saxutils.escape(content),'</%s>' % name, ''))
       
-   def tags_get(self):
+   def tags_get(self, color_set):
+      color_set.add(self.colorstring)
       return [self.tag_make(x, self.__dict__[x]) for x in ('name', 'description')] + self.tags
       
-   def markup_generate(self):
-      return '<Placemark>\n' + ''.join(self.tags_get()) + '</Placemark>\n'
+   def markup_generate(self, color_set):
+      return '<Placemark>\n' + ''.join(self.tags_get(color_set)) + '</Placemark>\n'
 
-   __str__ = markup_generate
 
 class KMLPlacemarkNode(KMLPlacemark):
    def __init__(self, coord, *args, **kwargs):
       KMLPlacemark.__init__(self, *args, **kwargs)
       self.coord = coord
    
-   def tags_get(self):
-      return KMLPlacemark.tags_get(self) + \
-         ['<Point>%s</Point>\n' % self.tag_make('coordinates', '%f,%f,0' % (self.coord.longitude, self.coord.latitude))] + \
-         ['<Style><LabelStyle>%s</LabelStyle></Style>\n' % self.tag_make('color', self.colorstring, False)]
-      
+   def tags_get(self, color_set):
+      return KMLPlacemark.tags_get(self, color_set) + \
+         [self.tag_make('styleUrl', '#col%s' % (self.colorstring,), False) + '\n'] + \
+         ['<Point>%s</Point>\n' % self.tag_make('coordinates', '%f,%f,0' % (
+            self.coord.longitude, self.coord.latitude))]
+
 
 class KMLPlacemarkEdge(KMLPlacemark):
    def __init__(self, coord1, coord2, *args, **kwargs):
@@ -59,10 +60,14 @@ class KMLPlacemarkEdge(KMLPlacemark):
       self.coord1 = coord1
       self.coord2 = coord2
       
-   def tags_get(self):
-      return KMLPlacemark.tags_get(self) + ['<LineString>\n%s</LineString>\n' % 
-         self.tag_make('coordinates', '%f,%f,0\n%f,%f,0' % (self.coord1.longitude, self.coord1.latitude, self.coord2.longitude, self.coord2.latitude))] + \
-         ['<Style><LineStyle>%s<width>2</width></LineStyle></Style>\n' % self.tag_make('color', self.colorstring, False)]
+   def tags_get(self, color_set):
+      return KMLPlacemark.tags_get(self, color_set) + \
+         [self.tag_make('styleUrl', '#col%s' % (self.colorstring,), False) + '\n'] + \
+         ['<LineString>\n%s</LineString>\n' % 
+         self.tag_make('coordinates', '%f,%f,0\n%f,%f,0' % (
+            self.coord1.longitude, self.coord1.latitude, self.coord2.longitude,
+            self.coord2.latitude))]
+         
 
 
 class KMLFileWriter(LQColorMap):
@@ -97,8 +102,11 @@ class KMLFileWriter(LQColorMap):
          self.edgetags.append(KMLPlacemarkEdge(coord1=coord1, coord2=coord2, lq=lq, colorstring=self.color_calculate(lq), name="%s %s" % (node1_ip, node2_ip), description=str(lq))) 
 
    def markup_generate(self):
+      color_set = set()
+      ne_tags = [x.markup_generate(color_set) for x in (self.nodetags + self.edgetags)]
+      color_style_tags = ['<Style id="col%s"><LabelStyle><color>%s</color></LabelStyle></Style>\n' % (cs, cs) for cs in color_set]
       return '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://earth.google.com/kml/2.1">\n<Document>\n' + \
-             ''.join([str(x) for x in (self.nodetags + self.edgetags)]) + '</Document>\n</kml>\n'
+             ''.join(color_style_tags) + ''.join(ne_tags) + '</Document>\n</kml>\n'
 
    def save(self, filename):
       file(filename, 'w').write(self.markup_generate())
