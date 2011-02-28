b9fd44d315841b9f3693703c55c8d8002594b849
rist <rist>
2011-02-28 19:40:01 +0000

    impl

--- sources/oni_management_portal/src/de/oni/portal/persistence/PersistenceProvider.java
+++ sources/oni_management_portal/src/de/oni/portal/persistence/PersistenceProvider.java
@@ -16,24 +16,42 @@ public class PersistenceProvider implements PersistenceConstants
 {
 	
 	private static final Logger log = Logger.getLogger(PersistenceProvider.class);
+	
+	private static EntityManagerFactory emFactory;
+
+	protected static EntityManagerFactory getEntityManagerFactory()
+	{
+		if (emFactory == null)
+		{
+			emFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, getPersistenceOverrides());
+		}
+		return emFactory;
+	}
+	
+	protected static Map<String, String> getPersistenceOverrides()
+	{
+		Map<String, String> overrides = new HashMap<String, String>();
+		final String url = Config.getString("portal.persistence.jdbc.url", null);
+		if (url != null) overrides.put(PersistenceUnitProperties.JDBC_URL, url);
+		final String usr = Config.getString("portal.persistence.jdbc.user", null);
+		if (usr != null) overrides.put(PersistenceUnitProperties.JDBC_USER, usr);
+		final String drv = Config.getString("portal.persistence.jdbc.driver", null);
+		if (drv != null) overrides.put(PersistenceUnitProperties.JDBC_DRIVER, drv);
+		final String pwd = Config.getString("portal.persistence.jdbc.password", null);
+		if (pwd != null) overrides.put(PersistenceUnitProperties.JDBC_PASSWORD, pwd);
+		return overrides;
+	}
 
 	public static EntityManager getEntityManager()
 	{
-		return getEntityManager(null);
+		return getEntityManagerFactory().createEntityManager();
 	}
 	
 	protected static EntityManager getEntityManager(Map<String, String> params)
 	{
-		Map<String, String> overrideParams = params != null ? new HashMap<String, String>(params) : new HashMap<String, String>();
-		String url = Config.getString("portal.persistence.jdbc.url", null);
-		if (url != null) overrideParams.put(PersistenceUnitProperties.JDBC_URL, url);
-		String usr = Config.getString("portal.persistence.jdbc.user", null);
-		if (usr != null) overrideParams.put(PersistenceUnitProperties.JDBC_USER, usr);
-		String drv = Config.getString("portal.persistence.jdbc.driver", null);
-		if (drv != null) overrideParams.put(PersistenceUnitProperties.JDBC_DRIVER, drv);
-		String pwd = Config.getString("portal.persistence.jdbc.password", null);
-		if (pwd != null) overrideParams.put(PersistenceUnitProperties.JDBC_PASSWORD, pwd);
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, overrideParams);
+		Map<String, String> overrides = getPersistenceOverrides();
+		overrides.putAll(params);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, overrides);
 		EntityManager em = emf.createEntityManager();
 		return em;
 	}
@@ -41,13 +59,13 @@ public class PersistenceProvider implements PersistenceConstants
 	protected static void generateSqlScripts()
 	{
 		final String dir = Config.getString("portal.persistence.sqlscripts.targetfolder", System.getProperty("java.io.tmpdir"));
-		Map<String, String> persistProperties = new HashMap<String, String>();
-		persistProperties.put(PersistenceUnitProperties.DDL_GENERATION, PersistenceUnitProperties.DROP_AND_CREATE);
-		persistProperties.put(PersistenceUnitProperties.DDL_GENERATION_MODE, PersistenceUnitProperties.DDL_SQL_SCRIPT_GENERATION);
-		persistProperties.put(PersistenceUnitProperties.APP_LOCATION, dir);
-		persistProperties.put(PersistenceUnitProperties.CREATE_JDBC_DDL_FILE, PERSISTENCE_CREATE_DDL);
-		persistProperties.put(PersistenceUnitProperties.DROP_JDBC_DDL_FILE, PERSISTENCE_DROP_DDL);
-		getEntityManager(persistProperties).close();
+		Map<String, String> persistenceProperties = new HashMap<String, String>();
+		persistenceProperties.put(PersistenceUnitProperties.DDL_GENERATION, PersistenceUnitProperties.DROP_AND_CREATE);
+		persistenceProperties.put(PersistenceUnitProperties.DDL_GENERATION_MODE, PersistenceUnitProperties.DDL_SQL_SCRIPT_GENERATION);
+		persistenceProperties.put(PersistenceUnitProperties.APP_LOCATION, dir);
+		persistenceProperties.put(PersistenceUnitProperties.CREATE_JDBC_DDL_FILE, PERSISTENCE_CREATE_DDL);
+		persistenceProperties.put(PersistenceUnitProperties.DROP_JDBC_DDL_FILE, PERSISTENCE_DROP_DDL);
+		getEntityManager(persistenceProperties).close();
 		log.info(String.format("sql scripts written to %s", dir));
 	}
 
