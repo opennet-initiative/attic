b346ef03d3eb9ca5a74fea80f601292d87ef9fc4
meisser <meisser>
2012-02-12 09:03:26 +0000

    verbessertes Admin interface

--- sources/oni_manager/oni_manager/oni_manager_app/admin.py
+++ sources/oni_manager/oni_manager/oni_manager_app/admin.py
@@ -9,6 +9,20 @@ from models import Anschrift
 from models import Mitglied
 from django.contrib import admin
 
+#class AnschriftInline(admin.StackedInline):
+#    model = Anschrift
+
+
+class MitgliedAdmin(admin.ModelAdmin):
+#    inlines = [AnschriftInline,]
+    list_display = ('nickname', 'status','mitgliedseit',"foto")
+    list_filter = ("status", 'mitgliedseit')
+    ordering = ('status',)
+    search_fields = ('nickname',)
+    exclude=("user",)
+    
+    
+
 admin.site.register(Bankkonto)
 admin.site.register(Anschrift)
-admin.site.register(Mitglied)
+admin.site.register(Mitglied, MitgliedAdmin)
