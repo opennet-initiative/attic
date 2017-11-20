9a666d970dc2a4f017117aa8b54da04244ebbf33
Mathias Mahnke <ap27@opennet-initiative.de>
2017-11-20 05:23:37 +0100

    prepare on_firmware for import

--- tags/release-0.11ipkg-3/etc/openvpn/opennet_user/opennet_down.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-eval $(/usr/bin/netparam)
-iptables -t nat -D POSTROUTING -o $dev -s $LANNET/$LANPRE -j SNAT --to-source $ifconfig_local
-iptables -t nat -D POSTROUTING -o $dev -s $DHCPWIFINET/$DHCPWIFIPRE -j SNAT --to-source $ifconfig_local
-rm -f /tmp/openvpn_msg.txt	# remove running message
--- tags/release-0.11ipkg-8/www/cgi-bin/webif/opennet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo Content-type: text/html
-echo
-echo "<html><head>"
-echo "<meta http-equiv=\"refresh\" content=\"0; URL=/cgi-bin/index.html\">"
-echo "</head></html>"
-##WEBIF:name:Opennet-Interface:1:Opennet
